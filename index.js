f39ed1522008bb19ca7a8c295f1eac7c2976a537
vishalicious213 <vishalicious213@gmail.com>
2023-01-26 14:55:38 -0800

    tweet button logs out contents of tweetInput

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { tweetsData } from "./data.js"
 
-console.log("Twimba")
-console.log(tweetsData)
+const tweetInput = document.getElementById("tweet-input")
+const tweetBtn = document.getElementById("tweet-btn")
+
+tweetBtn.addEventListener("click", function() {
+    console.log(tweetInput.value)
+})
