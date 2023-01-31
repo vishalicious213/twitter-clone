97f8a01c0fceb03abefe8586ed5727d0dd214f08
vishalicious213 <vishalicious213@gmail.com>
2023-01-30 19:22:58 -0800

    updated tweetsData

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ export const tweetsData = [
         profilePic: `images/headshot4.jpg`,
         likes: 213,
         retweets: 153,
-        tweetText: `Click a tweet's text to reply`,
+        tweetText: `Click a tweet's text to reply / New tweets are saved locally`,
         replies: [
             {
                 handle: `@JeffBerlin ✅`,
